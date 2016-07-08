a174487dbb47670bec7668e442f7bc7c5585e408
pedro almeida <idlol2@hotmail.com>
2016-07-08 11:40:32 -0300

    Primeiro Commit

---
+++ room.js
@@ -0,0 +1,25 @@
+/**
+ * Created by LAWS on 08/07/2016.
+ */
+function Nsala(){
+    
+}
+function act(){
+
+}
+function xmlLoader(url){
+    /*
+    if(window.XMLHttpRequest){
+        var Loader = new XMLHttpRequest();
+        Loader.open("GET", url ,false);
+        Loader.send(null);
+        return Loader.responseXML;
+    }else if(window.ActiveXObject){
+        var Loader = new ActiveXObject("Msxml2.DOMDocument.3.0");
+        Loader.async = false;
+        Loader.load(url);
+        return Loader;
+    }
+    */
+    
+}
