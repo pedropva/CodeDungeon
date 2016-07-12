141f4d9926dd253488d867373d2bfd1685426ff6
pedro almeida <idlol2@hotmail.com>
2016-07-12 09:48:50 -0300

    Já puxando as coisas do xml e protoptipo de comandos

--- room.js
+++ room.js
@@ -1,25 +1,37 @@
 /**
  * Created by LAWS on 08/07/2016.
  */
-function Nsala(){
-    
-}
-function act(){
 
-}
-function xmlLoader(url){
-    /*
-    if(window.XMLHttpRequest){
-        var Loader = new XMLHttpRequest();
-        Loader.open("GET", url ,false);
-        Loader.send(null);
-        return Loader.responseXML;
-    }else if(window.ActiveXObject){
-        var Loader = new ActiveXObject("Msxml2.DOMDocument.3.0");
-        Loader.async = false;
-        Loader.load(url);
-        return Loader;
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+        //xmlLoader(xhttp);
+        nSala(xhttp);
+        descriptionRoom(xhttp);
     }
-    */
-    
+};
+
+xhttp.open("GET", "https://dl.dropboxusercontent.com/u/85412057/room1.xml", true);
+xhttp.send();
+
+function xmlLoader(xml){
+    var xmlDoc = xml.responseXML;
+    document.getElementById("descriptionRoom").innerHTML =
+        xmlDoc.getElementsByTagName("NumberRoom")[0].childNodes[0].nodeValue;
+}
+function nSala(xml){
+    var xmlDoc = xml.responseXML;
+    document.getElementById("roomNumber").innerHTML = "Sala: " +
+        xmlDoc.getElementsByTagName("room")[0].attributes.getNamedItem("id").nodeValue;
+}
+function descriptionRoom(xml){
+    var xmlDoc = xml.responseXML;
+    document.getElementById("descriptionRoom").innerHTML =
+        xmlDoc.getElementsByTagName("descriptionRoom")[0].childNodes[0].nodeValue;
+}
+
+function act(){
+    var texto;
+    texto = document.getElementById("texto").value;
+    document.getElementById("resposta").innerHTML = "Ola "+texto+"!";
 }
