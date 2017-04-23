57376041662adde0ee49de3bf1aa062e6be23adf
Kassio Sousa <contato@kassiosousa.com.br>
2017-04-23 19:25:28 -0300

    sql do banco

--- ci-restserver/application/migrations/001.sql
+++ ci-restserver/application/migrations/001.sql
@@ -1,17 +1,3 @@
-CREATE TABLE IF NOT EXISTS `usuarios` (
-	`pmk_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  `fok_room_atual` int(11) NOT NULL,
-  `user_usuario` varchar(100) NOT NULL,
-  `user_senha` varchar(255) NOT NULL,
-  `user_dtCriacao` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER,
-  `user_is_ativo` enum('Nao','Sim') NOT NULL DEFAULT 'Sim',
-  PRIMARY KEY (`pmk_usuario`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
----
-    ALTER TABLE `usuarios` ADD UNIQUE(`user_usuario`);
----
-
 CREATE TABLE IF NOT EXISTS `bosses` (
   `pmk_boss` int(11) NOT NULL AUTO_INCREMENT,
   `fok_room` int(11) NOT NULL,
@@ -20,9 +6,7 @@ CREATE TABLE IF NOT EXISTS `bosses` (
   `boss_is_ativo` enum('Nao','Sim') NOT NULL DEFAULT 'Sim',
   PRIMARY KEY (`pmk_boss`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 ---
-
 CREATE TABLE IF NOT EXISTS `itens` (
   `pmk_item` int(11) NOT NULL AUTO_INCREMENT,
   `fok_room` int(11) NOT NULL,
@@ -31,9 +15,7 @@ CREATE TABLE IF NOT EXISTS `itens` (
   `item_is_ativo` enum('Nao','Sim') NOT NULL DEFAULT 'Sim',
   PRIMARY KEY (`pmk_item`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 ---
-
 CREATE TABLE IF NOT EXISTS `logs` (
   `pmk_log` int(11) NOT NULL AUTO_INCREMENT,
   `fok_usuario` int(11) NOT NULL,
@@ -41,9 +23,7 @@ CREATE TABLE IF NOT EXISTS `logs` (
   `log_descricao` text NOT NULL,
   PRIMARY KEY (`pmk_log`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 ---
-
 CREATE TABLE IF NOT EXISTS `rooms` (
   `pmk_room` int(11) NOT NULL AUTO_INCREMENT,
   `room_numero` int(11) NOT NULL,
@@ -51,9 +31,18 @@ CREATE TABLE IF NOT EXISTS `rooms` (
   `room_is_ativo` enum('Nao','Sim') NOT NULL DEFAULT 'Sim',
   PRIMARY KEY (`pmk_room`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 ---
-
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `pmk_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `fok_room_atual` int(11) NOT NULL,
+  `user_usuario` varchar(100) NOT NULL,
+  `user_senha` varchar(255) NOT NULL,
+  `user_dtCriacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_is_ativo` enum('Nao','Sim') NOT NULL DEFAULT 'Sim',
+  PRIMARY KEY (`pmk_usuario`),
+  UNIQUE KEY `user_usuario` (`user_usuario`)
+) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+---
 CREATE TABLE IF NOT EXISTS `usuario_bosses` (
   `pmk_usuario_boss` int(11) NOT NULL AUTO_INCREMENT,
   `fok_usuario` int(11) NOT NULL,
@@ -61,9 +50,7 @@ CREATE TABLE IF NOT EXISTS `usuario_bosses` (
   `userboss_defeat` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`pmk_usuario_boss`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 ---
-
 CREATE TABLE IF NOT EXISTS `usuario_itens` (
   `pmk_useritem` int(11) NOT NULL AUTO_INCREMENT,
   `fok_usuario` int(11) NOT NULL,
@@ -72,9 +59,7 @@ CREATE TABLE IF NOT EXISTS `usuario_itens` (
   `useritem_room_drop` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`pmk_useritem`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 ---
-
 CREATE TABLE IF NOT EXISTS `usuario_rooms` (
   `pmk_userroom` int(11) NOT NULL AUTO_INCREMENT,
   `fok_usuario` int(11) NOT NULL,
